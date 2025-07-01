3051ba107837c0a80199be3f3d0038b3497d2888
Alfred Woodny Pierre <afredpw@gmail.com>
2025-07-01 05:14:30 -0400

    Update Welcome.tsx

--- frontend/src/components/HomePages/Welcome.tsx
+++ frontend/src/components/HomePages/Welcome.tsx
@@ -2,7 +2,7 @@ function Welcome()
 {
     return(
       <a href="https://github.com/tcode-g/ScreenBuddy" target="_blank" rel="noopener noreferrer">
-        <h1 className="title">ScreenBuddy</h1>
+        <h1 className="title">ScreenBuuddy</h1>
       </a>
 
       
