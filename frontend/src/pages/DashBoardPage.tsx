c8043c521072e397e7f65ecf6028f46fdcfc69be
Alfred Woodny Pierre <afredpw@gmail.com>
2025-07-01 05:19:56 -0400

    Update DashBoardPage.tsx

--- frontend/src/pages/DashBoardPage.tsx
+++ frontend/src/pages/DashBoardPage.tsx
@@ -1,6 +1,6 @@
 import PageTitle from '../components/DashboardPages/DashBoardTitle.tsx';
 import Logout from '../components/DashboardPages/Logout.tsx';
-import Profile from '../components/DashboardPages/profile.tsx';
+import Profile from '../components/DashboardPages/Profile.tsx';
 
 const DashBoardPage = () =>
 {
