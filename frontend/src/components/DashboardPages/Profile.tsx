34c1682c887660c4a09fc6a4634abe9400cda1e3
Luke <148372543+LukeSupan@users.noreply.github.com>
2025-07-03 14:40:07 -0400

    Attempted fix

--- frontend/src/components/DashboardPages/Profile.tsx
+++ frontend/src/components/DashboardPages/Profile.tsx
@@ -22,7 +22,7 @@ function Profile()
                 console.log(token);
                 console.log(userId);
                 console.log('header: ' , `Bearer ${token}`);
-                const response = await axios.get(`http://localhost:5000/api/profile/${userId}`, {headers: { Authorization: `Bearer ${token}`,}, });
+                const response = await axios.get(`http://172.233.171.46/:5000/api/profile/${userId}`, {headers: { Authorization: `Bearer ${token}`,}, });
                 console.log(response.data);
                 setProfile(response.data);
                 //console.log(profile.name + ' ' + profile.created);
