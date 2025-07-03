bc03715197fc8ad882e26f8275f5000b59ac1769
Luke <148372543+LukeSupan@users.noreply.github.com>
2025-07-03 11:44:28 -0400

    Undoing test push

--- frontend/src/pages/DashBoardPage.tsx
+++ frontend/src/pages/DashBoardPage.tsx
@@ -4,7 +4,6 @@ import DashBoardBody from '../components/DashboardPages/DashBoardBody.tsx';
 
 const DashBoardPage = () =>
 {
-  // test push to see if server resets
     return(
       <div>
         <DashBoardHeader />
