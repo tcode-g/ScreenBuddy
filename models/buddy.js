a88261938361b31edf4d2fa0070c7b4f9045e804
Darel <darelg2212.sc@gmail.com>
2025-07-18 20:26:40 -0400

    Added schema for buddies table

---
+++ models/buddy.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BuddySchema = new Schema({
+    // MongoDB has automatically creates a _id field for each
+
+    userID: {
+        type: ObjectId,
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    level: {
+        type: Number,
+        default: 1,
+        min: 1
+    },
+
+    xp: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+
+    xpToNextLevel: {
+        type: Number,
+        default: 10,
+        min: 0
+    },
+
+}, {timestamps: true});
+
+const Buddy = mongoose.model('buddy', BuddySchema, 'buddies');
+
+module.exports = Buddy;
