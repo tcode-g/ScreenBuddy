b5795cd2786bca51d10c787c82dd864de8542f4b
Darel Gomez <darelg2212.sc@gmail.com>
2025-07-21 00:11:32 -0400

    Added equip endpoint to the inventoryRoutes

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -6,7 +6,33 @@ const UserInventoryItem = require('../models/userInventory');
 
 router.post("/equip", auth, async (req, res) => {
   try {
-    
+    const { itemID } = req.body;
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        return res.status(400).json({ message: "Invalid user."});
+    }
+
+    const itemToEquip = await UserInventoryItem.findOne({
+      _id: itemID,
+      userId: user._id 
+    });
+
+    if (!itemToEquip) {
+      return res.status(404).json({ message: "Item not found in your inventory." });
+    }
+
+    const currentlyEquippedItem = await UserInventoryItem.findOne({
+      userId: userId,
+      isEquipped: true
+    });
+
+    if (currentlyEquippedItem) {
+        currentlyEquippedItem.isEquipped = false;
+        await currentlyEquippedItem.save();
+    }
+
+    itemToEquip.isEquipped = true;
+    await itemToEquip.save({ session });
 
     res.status(200).json({ message: "Item equipped successfully." });
   } catch (error) {
@@ -15,7 +41,7 @@ router.post("/equip", auth, async (req, res) => {
   }
 });
 
-router.get("/all", auth, async (req, res) => {
+router.get("/inventory", auth, async (req, res) => {
 try {
     const user = await User.findById(req.user.id);
     if (!user) {
