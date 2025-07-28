836d0906b4c220760be194d65a05a84d39785386
afredpw <afredpw@gmail.com>
2025-07-28 19:13:31 -0400

    shop

--- frontend/src/components/Buddies.tsx
+++ frontend/src/components/Buddies.tsx
@@ -1,11 +1,88 @@
 // map of buddys
 import defaultbuddy from '../assets/defaultdefault1.png';
+import triangleWhite from '../assets/buddies/triangle/triangleWhite.png';
+import triangleRed from '../assets/buddies/triangle/triangleRed.png';
+import trianglePink from '../assets/buddies/triangle/trianglePink.png';
+import triangleOrange from '../assets/buddies/triangle/triangleOrange.png';
+import triangleGreen from '../assets/buddies/triangle/triangleGreen.png';
+import triangleBlue from '../assets/buddies/triangle/triangleBlue.png';
+import triangleBlack from '../assets/buddies/triangle/triangleBlack.png';
 
+import squareBlack from '../assets/buddies/square/squareBlack.png';
+import squareBlue from '../assets/buddies/square/squareBlue.png';
+import squareGreen from '../assets/buddies/square/squareGreen.png';
+import squareOrange from '../assets/buddies/square/squareOrange.png';
+import squarePink from '../assets/buddies/square/squarePink.png';
+import squareRed from '../assets/buddies/square/squareRed.png';
+import squareWhite from '../assets/buddies/square/squareWhite.png';
+
+import pentagonBlack from '../assets/buddies/pentagon/pentagonBlack.png';
+import pentagonBlue from '../assets/buddies/pentagon/pentagonBlue.png';
+import pentagonGreen from '../assets/buddies/pentagon/pentagonGreen.png';
+import pentagonOrange from '../assets/buddies/pentagon/pentagonOrange.png';
+import pentagonPink from '../assets/buddies/pentagon/pentagonPink.png';
+import pentagonRed from '../assets/buddies/pentagon/pentagonRed.png';
+import pentagonWhite from '../assets/buddies/pentagon/pentagonWhite.png';
+
+import hexagonBlack from '../assets/buddies/hexagon/hexagonBlack.png';
+import hexagonBlue from '../assets/buddies/hexagon/hexagonBlue.png';
+import hexagonGreen from '../assets/buddies/hexagon/hexagonGreen.png';
+import hexagonOrange from '../assets/buddies/hexagon/hexagonOrange.png';
+import hexagonRed from '../assets/buddies/hexagon/hexagonRed.png';
+import hexagonPink from '../assets/buddies/hexagon/hexagonPink.png';
+import hexagonWhite from '../assets/buddies/hexagon/hexagonWhite.png';
+
+import circleBlack from '../assets/buddies/circle/circleBlack.png';
+import circleBlue from '../assets/buddies/circle/circleBlue.png';
+import circleGreen from '../assets/buddies/circle/circleGreen.png';
+import circleOrange from '../assets/buddies/circle/circleOrange.png';
+import circlePink from '../assets/buddies/circle/circlePink.png';
+import circleRed from '../assets/buddies/circle/circleRed.png';
+import circleWhite from '../assets/buddies/circle/circleWhite.png';
 
 
 const buddies: Record<string, string> = {
     // add more later
-    defaultbuddy
+    defaultbuddy,
+    triangleBlack,
+    triangleBlue,
+    triangleGreen,
+    triangleOrange,
+    trianglePink,
+    triangleRed,
+    triangleWhite,
+
+    squareBlack,
+    squareBlue,
+    squareGreen,
+    squareOrange,
+    squarePink,
+    squareRed,
+    squareWhite,
+
+    pentagonBlack,
+    pentagonBlue,
+    pentagonGreen,
+    pentagonOrange,
+    pentagonPink,
+    pentagonRed,
+    pentagonWhite,
+
+    hexagonBlack,
+    hexagonBlue,
+    hexagonGreen,
+    hexagonOrange,
+    hexagonPink,
+    hexagonRed,
+    hexagonWhite,
+
+    circleBlack,
+    circleBlue,
+    circleGreen,
+    circleOrange,
+    circlePink,
+    circleRed,
+    circleWhite
 };
 
 export default buddies
--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -80,3 +80,58 @@ router.get("/shop", auth, async (req, res) => {
     res.status(500).json({ message: "An error occurred while fetching shop items." });
   }
 });
+
+// single use api to populate shop items database
+router.get("/populate", async (req, res) => {
+  try {
+    const items = [
+      { name: 'triangleBlack', price: 100 }, 
+      { name: 'triangleBlue', price: 100 },
+      { name: 'triangleGreen', price: 100 },
+      { name: 'triangleOrange', price: 100 },
+      { name: 'trianglePink', price: 100 },
+      { name: 'triangleRed', price: 100 },
+      { name: 'triangleWhite', price: 100 }, 
+
+      { name: 'squareBlack', price: 250 },
+      { name: 'squareBlue', price: 250 },
+      { name: 'squareGreen', price: 250 },
+      { name: 'squareOrange', price: 250 },
+      { name: 'squarePink', price: 250 },
+      { name: 'squareRed', price: 250 },
+      { name: 'squareWhite', price: 250 },
+
+      { name: 'pentagonBlack', price: 500 },
+      { name: 'pentagonBlue', price: 500 },
+      { name: 'pentagonGreen', price: 500 },
+      { name: 'pentagonOrange', price: 500 },
+      { name: 'pentagonPink', price: 500 },
+      { name: 'pentagonRed', price: 500 },
+      { name: 'pentagonWhite', price: 500 },
+
+      { name: 'hexagonBlack', price: 1000 },
+      { name: 'hexagonBlue', price: 1000 },
+      { name: 'hexagonGreen', price: 1000 },
+      { name: 'hexagonOrange', price: 1000 },
+      { name: 'hexagonPink', price: 1000 },
+      { name: 'hexagonRed', price: 1000 },
+      { name: 'hexagonWhite', price: 1000 },
+
+      { name: 'circleBlack', price: 2000 },
+      { name: 'circleBlue', price: 2000 },
+      { name: 'circleGreen', price: 2000 },
+      { name: 'circleOrange', price: 2000 },
+      { name: 'circlePink', price: 2000 },
+      { name: 'circleRed', price: 2000 },
+      { name: 'circleWhite', price: 2000 }
+    ];
+
+    const data = await ShopItem.insertMany(items);
+    res.status(200).json({ message: "Shop items added successfully.", items: items });
+
+
+  } catch (error) {
+    console.error("error has occured", error);
+    res.status(500).json({message: "An error has occured" , "error": error });
+  }
+})
